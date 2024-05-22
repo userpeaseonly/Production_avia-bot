2620fc4691e0683b156ddedcef05e5081a72867d
userpeaseonly <userpeaseonly@gmail.com>
2024-05-22 21:16:09 +0500

    Fixing media files serving 404 error

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+
 from .serializers import NewsSerializer, UserSerializer
 from .models import New
 from rest_framework import generics, permissions, status
@@ -8,29 +10,29 @@ from django.contrib.auth import authenticate
 
 
 class UserLoginView(APIView):
-     def post(self, request):
-          username = request.data.get('username')
-          password = request.data.get('password')
-          user = authenticate(username=username, password=password)
-          if user:
-               serializer = UserSerializer(user)
-               return redirect('/admin/')
-          else:
-               return Response({'error': 'Invalid credentials!'}, status=status.HTTP_401_UNAUTHORIZED)
+    @csrf_exempt
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            serializer = UserSerializer(user)
+            return redirect('/admin/')
+        else:
+            return Response({'error': 'Invalid credentials!'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-class NewsList(generics.ListAPIView):
-     queryset = New.objects.all()
-     serializer_class = NewsSerializer
-     permission_classes = [permissions.AllowAny]
+class NewsList(generics.ListCreateAPIView):
+    queryset = New.objects.all()
+    serializer_class = NewsSerializer
 
 
 class NewsDetail(APIView):
-     def get(self, request, pk):
-          try:
-               news = New.objects.get(pk=pk)
-               serializer = NewsSerializer(news)
-               permission_classes = [permissions.AllowAny]
-               return Response(serializer.data)
-          except news.DoesNotExist:
-               return Response(status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, pk):
+        try:
+            news = New.objects.get(pk=pk)
+            serializer = NewsSerializer(news)
+            permission_classes = [permissions.AllowAny]
+            return Response(serializer.data)
+        except news.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
